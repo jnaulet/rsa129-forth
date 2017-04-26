758b8766490af50f172d0d04ca208f0a48279ed0
Joachim Naulet <jnaulet@rdinnovation.fr>
2017-04-26 11:28:33 +0200

    Added some files

---
+++ math.fs
@@ -0,0 +1,24 @@
+\ Simple math ops
+: pow2+ ( n p -- n^p where p>1 )
+    \ n p -- over
+    \ n p n -- swap
+    \ n n p -- 1 do
+    \ n n -- over
+    \ n n n -- *
+    \ n n^i -- loop
+    \ n n^p -- swap
+    \ n^p n -- drop
+    \ n^p
+    over swap 1 do over * loop swap drop ;
+
+: pow ( n p -- n^p )
+    \ n p -- dup
+    \ n p p -- 1 > if then
+    \ n p -- pow2+
+    \ n^p
+    \ else
+    \ n p -- drop
+    \ n^1
+    dup 1 > if pow2+ else drop then ;
+: square ( n -- n^2) dup * ;
+: ?odd ( n -- bool ) 1 and ;
---
+++ rnd.fs
@@ -0,0 +1,6 @@
+\ pseudo-random number generation
+\ John Von Neumann middle-square method
+
+include math.fs
+
+:
